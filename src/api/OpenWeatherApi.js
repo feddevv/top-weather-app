f8e3400acdda2c734a4573982f1076614379bf3f
Fedya Styopochkin <fst20071221@gmail.com>
2026-03-15 09:01:51 +0000

    Add search button and update styles for search functionality

---
+++ src/api/OpenWeatherApi.js
@@ -0,0 +1,17 @@
+export default class OpenWeatherApi {
+  #API_KEY = '8bdaac437edf6b913c8a7e41d3528a88'
+
+  async getCurrentWeather(city) {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.#API_KEY}`,
+    )
+
+    if (!response.ok) {
+      throw new Error(`The error occurred! Status code: ${response.status}`)
+    }
+
+    const data = await response.json()
+
+    console.log(data)
+  }
+}
